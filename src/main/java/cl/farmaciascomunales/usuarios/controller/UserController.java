f94bfb6f18918176eb493129a4ef7cb6107beb20
Rene-Yanez <rene.yanez2001@alumnos.ubiobio.cl>
2025-06-02 07:54:31 -0400

    Update UserController.java

--- src/main/java/cl/farmaciascomunales/usuarios/controller/UserController.java
+++ src/main/java/cl/farmaciascomunales/usuarios/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/usuarios")
+@RequestMapping("/usuarios")
 public class UserController {
 
     private final UserService service;
